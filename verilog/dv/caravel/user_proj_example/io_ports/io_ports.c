6aad1c603a77bc2b2b850d6ba072027a88eb64bd
manarabdelaty <manarabdelatty@aucegypt.edu>
2020-12-29 23:34:34 +0200

    Initial work on caravel + Ibex verification

--- verilog/dv/caravel/user_proj_example/io_ports/io_ports.c
+++ verilog/dv/caravel/user_proj_example/io_ports/io_ports.c
@@ -41,20 +41,81 @@ void main()
 
 	*/
 
-	// Configure lower 8-IOs as user output
-	// Observe counter value in the testbench
-	reg_mprj_io_0 =  GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_1 =  GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_2 =  GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_3 =  GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_4 =  GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_5 =  GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_6 =  GPIO_MODE_USER_STD_OUTPUT;
-	reg_mprj_io_7 =  GPIO_MODE_USER_STD_OUTPUT;
+	// GPIOs
+	reg_mprj_io_0 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_1 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_2 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_3 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_4 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_5 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_6 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_7 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
 
-        /* Apply configuration */
-        reg_mprj_xfer = 1;
-        while (reg_mprj_xfer == 1);
+	reg_mprj_io_8  =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_9  =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_10 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_11 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_12 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_13 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+
+	// Flash
+	reg_mprj_io_14 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_15 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_16 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_17 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+
+	// Flash CLK and Enable
+	reg_mprj_io_18 =  GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_19 =  GPIO_MODE_USER_STD_OUTPUT;
+
+	// UART 0
+	reg_mprj_io_20 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
+	reg_mprj_io_21 =  GPIO_MODE_USER_STD_OUTPUT;
+
+	// UART 1
+	reg_mprj_io_22 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
+	reg_mprj_io_23 =  GPIO_MODE_USER_STD_OUTPUT;
+
+	// SPI0
+	reg_mprj_io_24 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
+	reg_mprj_io_25 =  GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_26 =  GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_27 =  GPIO_MODE_USER_STD_OUTPUT;
+
+	// SPI1
+	reg_mprj_io_28 =  GPIO_MODE_USER_STD_INPUT_NOPULL;
+	reg_mprj_io_29 =  GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_30 =  GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_31 =  GPIO_MODE_USER_STD_OUTPUT;
+
+	// I2C0
+	reg_mprj_io_32 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_33 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	
+	// I2C1
+	reg_mprj_io_34 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+	reg_mprj_io_35 =  GPIO_MODE_USER_STD_BIDIRECTIONAL;
+
+	// PWM
+	reg_mprj_io_36 =  GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_37 =  GPIO_MODE_USER_STD_OUTPUT;
+
+	/* Apply configuration */
+	reg_mprj_xfer = 1;
+	while (reg_mprj_xfer == 1);
+
+	// Logic probes: configure LA[9:0] as outputs from the mgmt area
+	/*
+		LA       | Ibex
+
+		la[23:0] | CLK_DIV | 24'h000064
+		la[24]   | NMI     | 1'b0
+		la[25]   | EXT_IRQ | 1'b0
+		la[26]   | HResetn | 1'b1
+	*/
+
+	reg_la0_ena = 0x00000000;  
+	reg_la0_data = 0x4000064;
 
 }
 
--- verilog/dv/caravel/user_proj_example/io_ports/io_ports_tb.v
+++ verilog/dv/caravel/user_proj_example/io_ports/io_ports_tb.v
@@ -17,20 +17,98 @@
 
 `timescale 1 ns / 1 ps
 
-`include "caravel.v"
+`define   TEST_FILE   "../sw/test.hex" 
+`define   SIM_TIME    3000_000
+`define   SIM_LEVEL   0
+
+`define SOC_SETUP_TIME 800*2001
+
 `include "spiflash.v"
+`include "caravel.v"
+`include "sst26wf080b.v"
+`include "23LC512.v"
+
+`ifdef SIM
+
+`ifdef FAST
+    `define NO_DFFRAM
+    `define NO_HC_CACHE
+    `include "DFFRAM_beh.v"
+`else
+    `include "user_project/IPs/DFFRAM.v"
+	`include "user_project/IPs/DFFRAMBB.v"
+	`include "user_project/IPs/DMC_32x16HC.v"
+`endif
+
+`include "user_project/AHB_sys_0/AHBlite_sys_0.v"
+
+`include "user_project/AHB_sys_0/AHBlite_bus0.v"
+`include "user_project/AHB_sys_0/AHBlite_GPIO.v"
+`include "user_project/AHB_sys_0/AHBlite_db_reg.v"
+
+`include "user_project/AHB_sys_0/APB_sys_0/APB_WDT32.v"
+`include "user_project/AHB_sys_0/APB_sys_0/APB_TIMER32.v"
+`include "user_project/AHB_sys_0/APB_sys_0/APB_PWM32.v"
+`include "user_project/AHB_sys_0/APB_sys_0/AHB_2_APB.v"
+`include "user_project/AHB_sys_0/APB_sys_0/APB_bus0.v"
+`include "user_project/AHB_sys_0/APB_sys_0/APB_sys_0.v"
+
+`include "user_project/IPs/TIMER32.v"
+`include "user_project/IPs/PWM32.v"
+`include "user_project/IPs/WDT32.v"
+`include "user_project/IPs/spi_master.v"
+`include "user_project/IPs/i2c_master.v"
+`include "user_project/IPs/GPIO.v"
+`include "user_project/IPs/APB_UART.v"
+`include "user_project/IPs/APB_SPI.v"
+`include "user_project/IPs/APB_I2C.v"
+`include "user_project/IPs/AHBSRAM.v"
+`include "user_project/acc/AHB_SPM.v"
+
+`include "user_project/IPs/QSPI_XIP_CTRL.v"
+`include "user_project/IPs/RAM_3Kx32.v"
+
+`include "user_project/ibex/ibex_alu.v"
+`include "user_project/ibex/ibex_branch_predict.v"
+`include "user_project/ibex/ibex_compressed_decoder.v"
+`include "user_project/ibex/ibex_controller.v"
+`include "user_project/ibex/ibex_core.v"
+`include "user_project/ibex/ibex_counter.v"
+`include "user_project/ibex/ibex_cs_registers.v"
+`include "user_project/ibex/ibex_csr.v"
+`include "user_project/ibex/ibex_decoder.v"
+`include "user_project/ibex/ibex_dummy_instr.v"
+`include "user_project/ibex/ibex_ex_block.v"
+`include "user_project/ibex/ibex_fetch_fifo.v"
+`include "user_project/ibex/ibex_icache.v"
+`include "user_project/ibex/ibex_id_stage.v"
+`include "user_project/ibex/ibex_if_stage.v"
+`include "user_project/ibex/ibex_load_store_unit.v"
+`include "user_project/ibex/ibex_multdiv_fast.v"
+`include "user_project/ibex/ibex_multdiv_slow.v"
+`include "user_project/ibex/ibex_pmp.v"
+`include "user_project/ibex/ibex_prefetch_buffer.v"
+`include "user_project/ibex/ibex_register_file_latch.v"
+`include "user_project/ibex/ibex_wb_stage.v"
+`include "user_project/ibex/prim_clock_gating.v" 
+`include "user_project/ibex/ibex_register_file_ff.v"
+`include "user_project/ibex/ibex_wrapper.v"
+
+`include "user_project/soc_core.v"
+
+`endif
 
 module io_ports_tb;
 	reg clock;
-    	reg RSTB;
+    reg RSTB;
 	reg power1, power2;
 	reg power3, power4;
 
-    	wire gpio;
-    	wire [37:0] mprj_io;
+	wire gpio;
+	wire [37:0] mprj_io;
 	wire [7:0] mprj_io_0;
 
-	assign mprj_io_0 = mprj_io[7:0];
+	// assign mprj_io_0 = mprj_io[7:0];
 
 	// External clock is used by default.  Make this artificially fast for the
 	// simulation.  Normally this would be a slow clock and the digital PLL
@@ -42,44 +120,49 @@ module io_ports_tb;
 		clock = 0;
 	end
 
+	// GPIO Loopback!
+    // wire [13:0] GPIO_PINS;
+    // generate
+    //     genvar i;
+    //     for(i=0; i<14; i=i+1)
+    //         assign GPIO_PINS[i] = GPIOOEN_Sys0_S2[i] ? GPIOOUT_Sys0_S2[i] : 1'bz;
+    // endgenerate
+
+    // assign GPIO_PINS[15:8] = GPIO_PINS[7:0];
+    // assign GPIOIN_Sys0_S2 = GPIO_PINS;
+
+	// Serial Terminal connected to UART0 TX*/
+    terminal term(.rx(mprj_io[21]));  // RsTx_Sys0_SS0_S0
+
+    // SPI SRAM connected to SPI0
+    wire SPI_HOLD = 1'b1;
+    M23LC512 SPI_SRAM(
+        .RESET(~RSTB),
+        .SO_SIO1(mprj_io[24]),  // MSI_Sys0_SS0_S2
+        .SI_SIO0(mprj_io[25]),  // MSO_Sys0_SS0_S2
+        .CS_N(mprj_io[26]),     // SSn_Sys0_SS0_S2
+        .SCK(mprj_io[27]),      // SCLK_Sys0_SS0_S2
+        .HOLD_N_SIO3(SPI_HOLD)
+	);
+
+	initial begin
+		// Load the application into the N5 flash memory
+		#1  $readmemh(`TEST_FILE, flash.I0.memory);
+		$display("---------Ibex Flash -----------");
+		$display("Memory[0]: %0d, Memory[1]: %0d, Memory[2]: %0d, Memory[3]: %0d", 
+            flash.I0.memory[0], flash.I0.memory[1], flash.I0.memory[2], flash.I0.memory[3]);
+	end
+
 	initial begin
 		$dumpfile("io_ports.vcd");
 		$dumpvars(0, io_ports_tb);
 
-		// Repeat cycles of 1000 clock edges as needed to complete testbench
-		repeat (25) begin
-			repeat (1000) @(posedge clock);
-			// $display("+1000 cycles");
-		end
-		$display("%c[1;31m",27);
-		$display ("Monitor: Timeout, Test Mega-Project IO Ports (RTL) Failed");
-		$display("%c[0m",27);
-		$finish;
-	end
-
-	initial begin
-	    // Observe Output pins [7:0]
-	    wait(mprj_io_0 == 8'h01);
-	    wait(mprj_io_0 == 8'h02);
-	    wait(mprj_io_0 == 8'h03);
-    	    wait(mprj_io_0 == 8'h04);
-	    wait(mprj_io_0 == 8'h05);
-            wait(mprj_io_0 == 8'h06);
-	    wait(mprj_io_0 == 8'h07);
-            wait(mprj_io_0 == 8'h08);
-	    wait(mprj_io_0 == 8'h09);
-            wait(mprj_io_0 == 8'h0A);   
-	    wait(mprj_io_0 == 8'hFF);
-	    wait(mprj_io_0 == 8'h00);
-
-	    $display("Monitor: Test 1 Mega-Project IO (RTL) Passed");
-	    $finish;
-	end
-
-	initial begin
 		RSTB <= 1'b0;
 		#2000;
 		RSTB <= 1'b1;	    // Release reset
+		#(`SOC_SETUP_TIME);
+		#(`SIM_TIME);
+	    $finish;
 	end
 
 	initial begin		// Power-up sequence
@@ -95,10 +178,6 @@ module io_ports_tb;
 		power3 <= 1'b1;
 		#200;
 		power4 <= 1'b1;
-	end
-
-	always @(mprj_io) begin
-		#1 $display("MPRJ-IO state = %b ", mprj_io[7:0]);
 	end
 
 	wire flash_csb;
@@ -129,7 +208,7 @@ module io_ports_tb;
 		.vssd2	  (VSS),
 		.clock	  (clock),
 		.gpio     (gpio),
-        	.mprj_io  (mprj_io),
+        .mprj_io  (mprj_io),
 		.flash_csb(flash_csb),
 		.flash_clk(flash_clk),
 		.flash_io0(flash_io0),
@@ -147,6 +226,34 @@ module io_ports_tb;
 		.io2(),			// not used
 		.io3()			// not used
 	);
+
+	/* N5 Flash */
+    sst26wf080b flash(
+        .SCK(mprj_io[18]),     // fsclk
+        .SIO(mprj_io[17:14]),  // fdo
+        .CEb(mprj_io[19])      // fcen
+    );
+
+endmodule
+
+module terminal #(parameter bit_time = 400) (input rx);
+
+    integer i;
+    reg [7:0] char;
+    initial begin
+        forever begin
+            @(negedge rx);
+            i = 0;
+            char = 0;
+            #(3*bit_time/2);
+            for(i=0; i<8; i=i+1) begin
+                char[i] = rx;
+                #bit_time;
+            end
+            $write("%c", char);
+        end
+    end
+
 
 endmodule
 `default_nettype wire
